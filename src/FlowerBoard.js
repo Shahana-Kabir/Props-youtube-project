9eae18e0a8df115fbe62814a26430c2300e4672d
Shahana Kabir <shahana.kabir@gmail.com>
2021-10-29 11:18:28 -0700

    new things added

--- src/FlowerBoard.js
+++ src/FlowerBoard.js
@@ -1,23 +1,45 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import FlowerCard from './FlowerCard';
+import flower1 from './flower1.jpg';
+import rose from './rose.jpg';
+import lily from './lily.jpg';
+import daisy from './daisy.jpg';
+import './flowerCard.css';
 
     let flowerDetails = [
-     {id: 4, name: 'Sunflower', para: 'Sunflower is a nice flower', base_experience: 62},
-     {id: 7, name: 'Rose', para: 'Rose is a nice flower', base_experience: 63},
-     {id: 11, name: 'Jasmine', para: 'Jasmine is a nice flower', base_experience: 72},
-     {id: 12, name: 'Daisy', para: 'Daisy is a nice flower', base_experience: 178},
+     {id: 4, name: 'Sunflower', para: 'Sunflower is a nice flower', flowerImage: flower1},
+     {id: 7, name: 'Rose', para: 'Rose is a nice flower', flowerImage: rose},
+     {id: 11, name: 'lily', para: 'Jasmine is a nice flower', flowerImage: lily},
+     {id: 12, name: 'Daisy', para: 'Daisy is a nice flower', flowerImage: daisy},
      
     ];
-  
+    
+    
 
-const FlowerBoard = ()=> {        
-         return(
+    
+
+
+//   const changePara = ()=>{
+//       return <p>
+//           Here is the details
+//       </p>
+
+//   }
+
+const FlowerBoard = ()=> {
+    const [details, setDetails] = useState('');
+    function readMore(){
+        setDetails('Here you go');
+        
+
+    };  
+                 return(
 
              <div>
                  <h2>FlowerBoard</h2>
                 
                 {flowerDetails.map((p)=>{
-                    return(<FlowerCard id = {p.id} name = {p.name} para = {p.para}/>)
+                    return(<FlowerCard key = {p.id} id = {p.id} name = {p.name} para = {p.para} flowerImage = {p.flowerImage} onClick = {readMore}/>)
                     
                 })}
                 </div>
--- src/FlowerCard.js
+++ src/FlowerCard.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import flower1 from './flower1.jpg';
-import './flowerCard.css';
+import React, { Component, useState } from 'react';
+
 
 const FlowerCard = (props)=> {
-      
         return(
             <div className = "flowers">
                
                 <div>
                  <h2>{props.name}</h2>
-                    <img className = "flowers__image" src = {flower1} alt = "flower" height = "200" width = "200"/>
-                     <p>{props.para}</p>
-                    <button>Read more</button>                
-            </div>
+                    <img className = "flowers__image" src = {props.flowerImage} alt = "flower" height = "200" width = "200"/>
+                     <p>{props.para}</p>      
+                     <button onClick = {props.onClick()}>Read more</button> 
+                                 
+                </div>
             </div>
         )
 
